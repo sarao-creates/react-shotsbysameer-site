d320336033b096a9d10d2e0d29a4944dbb1d7b6c
Sameer Rao <sarao@live.unc.edu>
2020-12-14 01:11:08 -0500

    decided against using material-ui

--- src/Image/index.js
+++ src/Image/index.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react';
 import './index.css';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
+
 
 export default function Image(props) {
     if (props.sideBool === true)
@@ -12,13 +8,6 @@ export default function Image(props) {
         return (
             <div  className="image-container">
                 <img className="leftImage" src={props.src}></img>
-                <Card>
-                    <CardHeader
-                        title="Shrimp and Chorizo Paella"
-                        subheader="September 14, 2016"
-                    />
-
-                </Card>
             </div>
         );
     }
