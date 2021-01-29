380c22b96d634c4e4b13e1ca69d5898d6e60c314
Sameer Rao <sarao@live.unc.edu>
2021-01-29 11:31:12 -0500

    Added photo gallery and design changes

---
+++ src/tileData.js
@@ -0,0 +1,395 @@
+import AntelopeCanyon from './Images/arizona.jpg';
+import EiffelTower from './Images/eiffel-tower.jpg';
+import Mural from './Images/mural.jpg';
+import VeniceBasilica from './Images/venice-basilica.jpg';
+import MachuPicchu from './Images/machupichu.jpg';
+import Bean from './Images/bean.jpg';
+import FerrisWheel from './Images/ferris-wheel.jpg';
+import BeachMumbai from './Images/beach-mumbai.jpg';
+import BeachMumbai2 from './Images/beach-mumbai-2.jpg';
+import Airplane from './Images/airplanes.jpg';
+import AirplaneWindow from './Images/airplane-window.jpg';
+import Arc from './Images/arc-de-triomp.jpg';
+import ArcView from './Images/arc-view.jpg';
+import Arch from './Images/arch-in-rome.jpg';
+import Bandra from './Images/bandra-worli.jpg';
+import Bandra2 from './Images/bandra-worli-2.jpg';
+import BigBen from './Images/big-ben.jpg';
+import CastleFlag from './Images/castle-flag.jpg';
+import ChicagoCoast from './Images/chicago-coast.jpg';
+import ChicagoSilo from './Images/chicago-sillouhette.jpg';
+import ChicagoSkyline from './Images/chicago-skyline.jpg';
+import ChicagoWater from './Images/chicago-water.jpg';
+import Duomo from './Images/church-florence.jpg';
+import CinqueTerre from './Images/cinque-terre.jpg';
+import SunEffect from './Images/cool-pic.jpg';
+import Dolphins from './Images/dolphins.jpg';
+import Dome from './Images/dome.jpg';
+import DistantEiffel from './Images/eiffel-disstant.jpg';
+import EiffelView from './Images/eiffel-tower-view.jpg';
+import GatewayOfIndia from './Images/gateway-of-india.jpg';
+import HotelTaj from './Images/hotel-taj-side.jpg';
+import HotelTaj2 from './Images/hotel-taj.jpg';
+import InsideRomanColl from './Images/inside-rc.jpg';
+import RealFake from './Images/interesting-art-outside-trump.jpg';
+import LondonEye from './Images/london-eye.jpg';
+import ItalianFlags from './Images/more-flags.jpg';
+import Parthenon from './Images/more-parthenon.jpg';
+import MumbaiBuilding from './Images/mumbai-classic.jpg'
+import MumbaiTrees from './Images/mumbai-palm-trees.jpg';
+import Municipal from './Images/municipal-building-mumbai.jpg';
+import Municipal2 from './Images/municipal-building.jpg';
+import Parthenon2 from './Images/parthenon.jpg';
+import PurpleFlower from './Images/potent-purple.jpg';
+import RaleighSign from './Images/raleigh-sign.jpg';
+import MumbaiInter from './Images/rare-intersection.jpg';
+import RomanColliseum from './Images/roman-colliseum.jpg';
+import RomanColliseum2 from './Images/roman.jpg';
+import Roots from './Images/roots-amine.jpg';
+import SacreCouer from './Images/sacre-couer.jpg';
+import SoccerPark from './Images/soccer-park.jpg';
+import SpanishSteps from './Images/spanish-steps.jpg';
+import TallestMumbai from './Images/tallest-building.jpg';
+import Trevi from './Images/trevi-fountain.jpg';
+import TrumpTowerSun from './Images/trump-tower-sunset.jpg';
+import VaticanView from './Images/vatican-view.jpg';
+import Vatican from './Images/vatican.jpg';
+import VictorHugoMon from './Images/victor-hugo-mon.jpg';
+import VirginiaBeach from './Images/virginia-beach.jpg';
+import Wali from './Images/wali.jpg';
+
+
+
+const tileData = [
+
+    // =========== France ============
+    {
+        img: EiffelTower,
+        title: "Eiffel Tower – Paris, France",
+        author: "Sameer",
+    },
+    {
+        img: EiffelView,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: Arc,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: ArcView,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: DistantEiffel,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: SacreCouer,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+
+    // =========== Italy ============
+    {
+        img: VeniceBasilica,
+        title: "Basilica di Santa Maria della Salute – Venice, Italy",
+        author: "Sameer",
+    },
+    {
+        img: CastleFlag,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: CinqueTerre,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: Duomo,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: InsideRomanColl,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: ItalianFlags,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: SunEffect,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: Parthenon,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: SpanishSteps,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: Trevi,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: Parthenon2,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: RomanColliseum,
+        title: "hi",
+        author: "Sameer",
+        cols: 3,
+    },
+    {
+        img: RomanColliseum2,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: Vatican,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: VaticanView,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: Dome,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: VictorHugoMon,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: Arch,
+        title: "hi",
+        author: "Sameer",
+    },
+    // =========== India ============
+    {
+        img: BeachMumbai,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: BeachMumbai2,
+        title: "Beach on Bandra-Worli Sea Link – Mumbai, India",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: Bandra,
+        title: "hi",
+        author: "Sameer",
+        cols: 3,
+    },
+    {
+        img: Bandra2,
+        title: "hi",
+        author: "Sameer",
+        cols: 3,
+    },
+    {
+        img: HotelTaj,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: HotelTaj2,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: MumbaiBuilding,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: MumbaiTrees,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: Municipal,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: Municipal2,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: MumbaiInter,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: Roots,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: TallestMumbai,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: GatewayOfIndia,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    // =========== Chicago ============
+    {
+        img: Bean,
+        title: "The Bean – Illinois, United States",
+        author: "Sameer",
+    },
+    {
+        img: ChicagoCoast,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: ChicagoSilo,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: ChicagoWater,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: RealFake,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: ChicagoSkyline,
+        title: "hi",
+        author: "Sameer",
+        cols: 3,
+    },
+    {
+        img: Dolphins,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: TrumpTowerSun,
+        title: "hi",
+        author: "Sameer",
+    },
+
+    // =========== Misc. ============
+    {
+        img: FerrisWheel,
+        title: "National Harbor Ferris Wheel – Maryland, United States",
+        author: "Sameer",
+    },
+    {
+        img: AntelopeCanyon,
+        title: "Antelope Canyon – Arizona, United States",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: Mural,
+        title: "Mural",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: MachuPicchu,
+        title: "Machu Picchu – Cuzco, Peru",
+        author: "Sameer",
+    },
+    {
+        img: Airplane,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: AirplaneWindow,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: BigBen,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: PurpleFlower,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: LondonEye,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: RaleighSign,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: SoccerPark,
+        title: "hi",
+        author: "Sameer",
+    },
+    {
+        img: VirginiaBeach,
+        title: "hi",
+        author: "Sameer",
+        cols: 2,
+    },
+    {
+        img: Wali,
+        title: "hi",
+        author: "Sameer",
+        cols: 3,
+    },
+];
+
+export default tileData;
